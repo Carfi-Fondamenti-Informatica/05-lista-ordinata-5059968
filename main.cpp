7d4c7b3156ece4522fd31e0cd3d5f71ac2f25f15
5059968 <128074987+5059968@users.noreply.github.com>
2023-04-30 15:11:05 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int lista[10];
+    for(int i=0;i<10;i++){
+        cin >> lista[i];
+    }
+    int a=0;
+    cin >> a;
+    int b=0;
+    int i=0;
+    for (;i<9;i++){
+    if (a<lista[i]){
+        cout<<a<<endl;
+        break;
+       }else{
+        cout<<lista[i]<<endl;
+    }}  for(b=i;b<9;b++){
+        cout<<lista[b]<<endl;
+    }
+    return 0;
 }
